0956166aafde88349a29f47fd7ee58531231b25c
seyoung5744 <seyoung574458@gmail.com>
2025-08-09 13:02:16 +0900

    create schedule project

---
+++ src/main/java/com/example/schedule/SpartaScheduleAppDevelopApplication.java
@@ -0,0 +1,13 @@
+package com.example.schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaScheduleAppDevelopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaScheduleAppDevelopApplication.class, args);
+    }
+
+}
