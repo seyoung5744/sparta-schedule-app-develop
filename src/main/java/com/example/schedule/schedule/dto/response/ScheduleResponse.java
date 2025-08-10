3364a277d2bcb34437d8f9d019e041cbc55f5a5e
seyoung5744 <seyoung574458@gmail.com>
2025-08-10 14:17:08 +0900

    refactor : 일정 응답 생성일, 수정일 추가

--- src/main/java/com/example/schedule/schedule/dto/response/ScheduleResponse.java
+++ src/main/java/com/example/schedule/schedule/dto/response/ScheduleResponse.java
@@ -1,25 +1,39 @@
 package com.example.schedule.schedule.dto.response;
 
 import com.example.schedule.schedule.entity.Schedule;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class ScheduleResponse {
 
     private final Long id;
     private final String writer;
     private final String title;
     private final String contents;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime modifiedAt;
+
+    @Builder
+    private ScheduleResponse(Long id, String writer, String title, String contents, LocalDateTime createdAt, LocalDateTime modifiedAt) {
+        this.id = id;
+        this.writer = writer;
+        this.title = title;
+        this.contents = contents;
+        this.createdAt = createdAt;
+        this.modifiedAt = modifiedAt;
+    }
 
     public static ScheduleResponse of(Schedule schedule) {
-        return new ScheduleResponse(
-                schedule.getId(),
-                schedule.getWriter(),
-                schedule.getTitle(),
-                schedule.getContents()
-        );
+        return ScheduleResponse.builder()
+                .id(schedule.getId())
+                .writer(schedule.getWriter())
+                .title(schedule.getTitle())
+                .contents(schedule.getContents())
+                .createdAt(schedule.getCreateAt())
+                .modifiedAt(schedule.getModifiedAt())
+                .build();
     }
 }
